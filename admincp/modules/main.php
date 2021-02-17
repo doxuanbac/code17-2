ffa264beb292cdd00074f8e92b7e875f7bc1b93f
Do Xuan Bac <doxuanbactanap2000@gmail.com>
2021-02-17 20:26:55 +0700

    first commit

---
+++ admincp/modules/main.php
@@ -0,0 +1,17 @@
+<div class = "clear"></div>
+<div class="main">
+    <?php
+        if(isset($_GET['action'])){
+            $tam = $_GET['action'];
+        }else{
+            $tam='';
+        }
+        if($tam=='quanlydanhmucsanpham'){
+            include("modules/quanlydanhmucsp/them.php");
+            include("modules/quanlydanhmucsp/lietke.php");
+        }
+        else{
+            include("modules/dashboard.php");
+        }
+    ?>
+</div>
